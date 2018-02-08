2b529cecf6ec5845e7e5055f7c661dc3e659d545
Judith Ramirez <judramirez@csumb.edu>
2018-02-08 06:05:24 +0000

    updating Files

--- Hw1/index.php
+++ Hw1/index.php
@@ -7,49 +7,48 @@
        <link href ="css/style.css" rel= "stylesheet" type= "text/css"/>
     </head>
     
- <body>
+    <body>
     
-    <header>
-    <h1>
-       Viruses
-    </h1> 
-    </header>
-    
-    <nav>
-        <hr/>
-        <a href = "index.php">Home</a>
-        <a href = "trojans.html">Trojan Horse</a>
-        <a href = "spyware.html">Spyware</a>
-        <a href = "events.html">The Virus Timeline</a>
-        <hr/>
-    </nav>
-    
-        <main>
-            <section id = "left-col">
-                         <img src="http://heidacomputers.com/wp-content/uploads/2016/02/Virus-e1430855923357-300x300.jpg" alt = "Virus" title = "Virus">
-            </section>
-                
-            <section id = "right-col"> 
-                <h2>  What is a Virus? </h2>
-                <p> Just like humans can get be affected by illneses and viruses, computers can </p> 
-                <p>also be affected. A computer virus is malicious software or malware that when</p> 
-                <p> operating on a computer, can damage or destroy the system software. Eventually </p>
-                <p> even sometimes making the computer unusable. There are multiple ways to get a virus</p>
-                <p>  on a computer. A few of the ways a virus spreads is by: </p>
-                
-                <ul> 
-                    <li>Email and text message attachments</li>
-                    <li>Internet file downloads</li>
-                    <li>Social media scam links</li>    
-                </ul>
-            </section>
-        </main>
-    
- </body>
+        <header>
+        <h1>
+           Viruses
+        </h1> 
+        </header>
         
-        
-        <footer>
+        <nav>
             <hr/>
-            <p><small> @Ramirez 2017 Disclaimer: This website is for educational purposes only</small></p>
-        </footer>
+            <a href = "index.php">Home</a>
+            <a href = "trojans.html">Trojan Horse</a>
+            <a href = "spyware.html">Spyware</a>
+            <a href = "events.html">The Virus Timeline</a>
+            <hr/>
+        </nav>
+        
+            <main>
+                <section id = "left-col">
+                             <img src="http://heidacomputers.com/wp-content/uploads/2016/02/Virus-e1430855923357-300x300.jpg" alt = "Virus" title = "Virus">
+                </section>
+                    
+                <section id = "right-col"> 
+                    <h2>  What is a Virus? </h2>
+                    <p> Just like humans can get be affected by illneses and viruses, computers can </p> 
+                    <p>also be affected. A computer virus is malicious software or malware that when</p> 
+                    <p> operating on a computer, can damage or destroy the system software. Eventually </p>
+                    <p> even sometimes making the computer unusable. There are multiple ways to get a virus</p>
+                    <p>  on a computer. A few of the ways a virus spreads is by: </p>
+                    
+                    <ul> 
+                        <li>Email and text message attachments</li>
+                        <li>Internet file downloads</li>
+                        <li>Social media scam links</li>    
+                    </ul>
+                </section>
+            </main>
+    
+    </body>
+        
+    <footer>
+        <hr/>
+        <p>@Ramirez 2017 Disclaimer: This website is for educational purposes only</p>
+    </footer>
 </html>
