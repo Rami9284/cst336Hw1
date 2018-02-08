b15911465a5c89904459a662a985f60f3812a111
Judith Ramirez <judramirez@csumb.edu>
2018-02-08 05:36:40 +0000

    Fixing

--- Hw1/index.php
+++ Hw1/index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-        <head>
+    <head>
         <meta charset= "utf-8">
         <title>Computer Viruses: What is a Virus</title>
        
@@ -16,43 +16,43 @@
         </header>
         
         <nav>
-            <hr width = "65%"/>
+            <hr/>
             <a href = "index.php">Home</a>
             <a href = "trojans.html">Trojan Horse</a>
             <a href = "spyware.html">Spyware</a>
             <a href = "events.html">The Virus Timeline</a>
-            <hr width = "65%"/>
+            <hr/>
         </nav>
         
-        <main>
-            
-        <section class = "left-col">
-                     <img src="http://heidacomputers.com/wp-content/uploads/2016/02/Virus-e1430855923357-300x300.jpg" title = "Virus">
-        </section>
-            
-        <section class = "right-col"> 
-                <h2>  What is a Virus? </h2>
-            <p> Just like humans can get be affected by illneses and viruses, computers can </p> 
-                <p>also be affected. A computer virus is malicious software or malware that when</p> 
-                <p> operating on a computer, can damage or destroy the system software. Eventually </p>
-                <p> even sometimes making the computer unusable. There are multiple ways to get a virus</p>
-                <p>  on a computer. A few of the ways a virus spreads is by: </p>
+            <main>
                 
-                <ul> 
-                    <li>Email and text message attachments</li>
-                    <li>Internet file downloads</li>
-                    <li>Social media scam links</li>    
-                </ul>
-
-        </section>
-    
-        </main>
+                 <section id = "left-col">
+                             <img src="http://heidacomputers.com/wp-content/uploads/2016/02/Virus-e1430855923357-300x300.jpg" title = "Virus">
+                </section>
+                    
+                <section id = "right-col"> 
+                        <h2>  What is a Virus? </h2>
+                    <p> Just like humans can get be affected by illneses and viruses, computers can </p> 
+                        <p>also be affected. A computer virus is malicious software or malware that when</p> 
+                        <p> operating on a computer, can damage or destroy the system software. Eventually </p>
+                        <p> even sometimes making the computer unusable. There are multiple ways to get a virus</p>
+                        <p>  on a computer. A few of the ways a virus spreads is by: </p>
+                        
+                        <ul> 
+                            <li>Email and text message attachments</li>
+                            <li>Internet file downloads</li>
+                            <li>Social media scam links</li>    
+                        </ul>
+        
+                </section>
+        
+            </main>
         
         </body>
         
         
         <footer>
-            <hr width = "65%" />
-            <small> <p>@Ramirez 2017</p> <p>Disclaimer: This website is for educational purposes only</p><small>
+            <hr/>
+            <p><small> @Ramirez 2017 Disclaimer: This website is for educational purposes only<small></p>
         </footer>
 </html>
